68d00cb8f21e51c61420e422d002f147b72adeee
matthcol <matthias.colin@gmail.com>
2023-07-26 14:20:40 +0200

    stats

--- moviecollection/src/test/java/org/example/data/demo/MovieStreamDemo.java
+++ moviecollection/src/test/java/org/example/data/demo/MovieStreamDemo.java
@@ -2,6 +2,7 @@ package org.example.data.demo;
 
 import org.example.data.Movie;
 import org.example.data.provider.MovieProvider;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +17,9 @@ class MovieStreamDemo {
     static void initData() {
         movieList = List.of(
                 new Movie("Barbie", 2023),
-                new Movie("Oppenheimer", 2023),
-                new Movie("The Batman", 2022),
-                new Movie("Star Wars: Episode IV - A New Hope", 1977),
+                new Movie("Oppenheimer", 2023, 180),
+                new Movie("The Batman", 2022, 176),
+                new Movie("Star Wars: Episode IV - A New Hope", 1977, 121),
                 new Movie("Indiana Jones and the Last Crusade", 1989)
         );
     }
@@ -109,6 +110,60 @@ class MovieStreamDemo {
                 .collect(Collectors.toCollection(TreeSet::new));
         System.out.println(titleSet);
         System.out.println();
+    }
+
+    @Test
+    void demoJoining() {
+        String titles = movieList.stream()
+                .map(Movie::getTitle)
+                .collect(Collectors.joining(", "));
+        System.out.println(titles);
+        System.out.println();
+    }
+
+    @Test
+    void demoStatistics(){
+        long nbMovie2023 = movieList.stream()
+                .filter(movie -> movie.getYear() == 2023)
+                .count();
+        System.out.println("Movies of year 2023: " + nbMovie2023);
+
+        OptionalInt minYear = movieList.stream()
+                .mapToInt(Movie::getYear)
+                .min();
+        System.out.println("Minimum year: " + minYear);
+
+        OptionalInt maxYear = movieList.stream()
+                .mapToInt(Movie::getYear)
+                .filter(year -> year < 1970)
+                .max();
+        System.out.println("Maximum year: " + maxYear);
+
+        var totalDuration = movieList.stream()
+                .filter(movie -> Objects.nonNull(movie.getDuration()))
+                .mapToInt(Movie::getDuration)
+                .sum();
+        System.out.println("Total duration: " + totalDuration + " mn");
+
+        IntSummaryStatistics stats = movieList.stream()
+                .filter(movie -> Objects.nonNull(movie.getDuration()))
+                .mapToInt(Movie::getDuration)
+                .summaryStatistics();
+        System.out.println(stats);
+
+        IntSummaryStatistics stats2 = movieList.stream()
+                .filter(movie -> movie.getYear() == 3035)
+                .mapToInt(Movie::getDuration)
+                .summaryStatistics();
+        System.out.println(stats2); // min = MAX_INT, max = MIN_INT, avg = 0.0 !
+    }
+
+    @Test
+    void extractDataFromEmptyOptional() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            Integer duration = null;
+            int durationPrimitive = duration;
+        });
     }
 
 }
