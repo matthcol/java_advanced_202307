0f7eb936b169425f1d1489d5b8f2850d63e3e9e8
matthcol <matthias.colin@gmail.com>
2023-07-26 14:28:32 +0200

    exo stream

---
+++ moviecollection/src/test/java/org/example/data/exercise/MovieStreamExercise.java
@@ -0,0 +1,33 @@
+package org.example.data.exercise;
+
+import org.example.data.Movie;
+import org.example.data.provider.MovieProvider;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MovieStreamExercise {
+
+    static List<Movie> movieList = MovieProvider.movieBigList();
+
+    @Test
+    void exerciseCountMoviesWithoutDuration(){
+        // TODO
+    }
+
+    @Test
+    void exerciseListMoviesFromYear1984(){
+        // TODO
+    }
+
+    @Test
+    void exerciseListTitlesOfMovieFromYear1984(){
+        // TODO
+    }
+
+    @Test
+    void exerciceListTitlesOfMovieBefore1950BeginningByR(){
+        // TODO
+    }
+
+}
