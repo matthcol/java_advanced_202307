f6b7174126772796f0e3f905b028eef478b2c4fc
matthcol <matthias.colin@gmail.com>
2023-07-28 11:39:27 +0200

    test equals and hashcode

--- moviecollection/src/test/java/org/example/data/tu/PersonTest.java
+++ moviecollection/src/test/java/org/example/data/tu/PersonTest.java
@@ -2,13 +2,29 @@ package org.example.data.tu;
 
 import org.example.data.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+
+    static List<Person> persons;
+
+    @BeforeAll
+    static void initData() {
+        persons = List.of(
+                new Person("Joe"),
+                new Person("Jack"),
+                new Person("William"),
+                new Person("Averell")
+        );
+    }
 
     @Test
     void testAllArgsConstructor(){
@@ -48,6 +64,34 @@ class PersonTest {
         assertAll(
                 () -> assertNull(person.getName(), "name"),
                 () -> assertNull(person.getBirthdate(), "birthdate")
+        );
+    }
+
+    @Test
+    void testEqualsCompatibleWithList(){
+        // given
+        var person = persons.get(2); // reference already in the list
+        var person2 = new Person("William"); // copy of a person already in the list
+        // when 1: search with a reference already in the list
+        var isInList = persons.contains(person);
+        // then 1:
+        assertTrue(isInList, "person in the list by reference");
+        // when 2:
+        var isInList2 = persons.contains(person2);
+        // then 2:
+        assertTrue(isInList2, "person in the list by copy");
+    }
+
+    @Test
+    void testEqualsHashCodeCompatibleWithHashSet(){
+        // when
+        var personSet = new HashSet<>(persons);
+        var person = persons.get(2); // reference already in the list/set
+        var person2 = new Person("William"); // copy of a person already in the list/set
+        // when/then
+        assertAll(
+                () -> assertTrue(personSet.contains(person), "person in the hashset by reference"),
+                () -> assertTrue(personSet.contains(person2), "person in the hashset by copy")
         );
     }
 }
